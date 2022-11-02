76a3332f808ae264f38f4cf5d9ebb73eec9f5b99
belen <anabelenarroyo21@gmail.com>
2022-11-02 15:50:46 -0500

    agregado método logout

--- my-app/src/app/services/auth.service.ts
+++ my-app/src/app/services/auth.service.ts
@@ -25,7 +25,6 @@ export class AuthService {
   constructor(
     private http: HttpClient,
     private tokenService: TokenService,
-    private router: Router
   ) { }
 
   login(email: string, password: string) {

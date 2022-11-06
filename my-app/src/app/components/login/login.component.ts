6bc088881b8a141248076d94452ba49895296ed2
belen <anabelenarroyo21@gmail.com>
2022-11-06 13:36:05 -0500

    modificado ruteo de la función login

--- my-app/src/app/components/login/login.component.ts
+++ my-app/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { UsersService } from '../../services/users.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,10 +13,12 @@ export class LoginComponent {
   email = ''
   password = ''
   statusDetail = ''
+  id = ''
 // tenemos que injectar en el contructor el servicio router
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private usersService: UsersService
   ) { }
 
   login() {
@@ -23,7 +26,26 @@ export class LoginComponent {
       this.authService.login(this.email, this.password)
       .subscribe(resp => {
         console.log(resp)
-        this.router.navigate(['/pedidos'])
+        this.token = resp.token
+        this.usersService.getAll(this.token)
+        .subscribe(resp =>{
+          for (let i of resp) {
+            if(i.email == this.email){
+              this.id = i.id
+            }
+          }
+          this.usersService.getRoles(this.token, this.id)
+          .subscribe(resp=>{
+            if(resp.roles.admin){
+              this.router.navigate(['/pedidos'])
+            }else if(resp.roles.waiter){
+              this.router.navigate(['/pedidos'])
+            }else if(resp.roles.chef){
+              this.router.navigate(['/ordenes'])
+            }
+          })
+
+        })
       },response =>{
         console.error(response);
         this.statusDetail = 'usuario o contraseña errada'})
